4fc834c344fc03673258ef069cc75d8d2d7349df
dingziyang <565444994@qq.com>
2018-11-08 13:45:25 +0800

    '去掉无用import'

--- src/main/java/com/dcm/more_ds/config/DataSourceConfig.java
+++ src/main/java/com/dcm/more_ds/config/DataSourceConfig.java
@@ -7,14 +7,11 @@ import com.dcm.more_ds.constant.DataSourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
